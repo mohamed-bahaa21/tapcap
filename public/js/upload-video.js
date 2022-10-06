1b029c11a34db6097460b41774d8ffa62e3c6092
Mohamed-Elzohary <MohamedElzohary2001@outlook.com>
2022-10-06 06:53:54 +0200

    updated: upload-video seperation of concerns

---
+++ public/js/upload-video.js
@@ -0,0 +1,31 @@
+var uploadDiv = document.getElementById("upload-video");
+var video = document.getElementById("video");
+var file = document.getElementById("file");
+
+var $droparea = $('#video_container');
+var $fileInput = $('#file');
+
+$fileInput.on('dragenter focus click', function () {
+    $droparea.addClass('drop-active');
+});
+
+// back to normal state
+$fileInput.on('dragleave blur drop', function () {
+    $droparea.removeClass('drop-active');
+});
+
+function dropHandler(e) {
+    e.preventDefault();
+    [].forEach.call(e.dataTransfer.files, file => {
+        uploadDiv.style.display = 'none';
+        video.style.display = 'block';
+        video.src = '/video/video.mp4';
+    });
+}
+file.onchange = function (e) {
+    // console.log(this.files[0]);
+    // console.log(this.value);
+    uploadDiv.style.display = 'none';
+    video.style.display = 'block';
+    video.src = '/video/video.mp4';
+};
